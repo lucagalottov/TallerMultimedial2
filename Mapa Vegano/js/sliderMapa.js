dce6b6d78caafbbd4622345ea61381c17d348a6c
lucagalottov <66344789+lucagalottov@users.noreply.github.com>
2021-12-03 20:44:32 -0300

    Add files via upload

---
+++ Mapa Vegano/js/sliderMapa.js
@@ -0,0 +1,11 @@
+var slider = document.getElementById("myRange");
+
+/*
+function changeStyle() {
+  document.getElementsByClassName("Argentina")[0].style.fill = "red";
+} */
+
+/*
+var ar = document.getElementByClassName("Argentina");
+console.log(ar);
+ar.setAttribute("style","fill: red"); */
