2f38f96ef924f780be03bb53aaa43d0aa3611c5a
lucagalottov <66344789+lucagalottov@users.noreply.github.com>
2021-12-05 15:24:13 -0300

    Add files via upload

--- Mapa Vegano/js/sliderMapa.js
+++ Mapa Vegano/js/sliderMapa.js
@@ -1,11 +1,28 @@
 var slider = document.getElementById("myRange");
 
-/*
-function changeStyle() {
-  document.getElementsByClassName("Argentina")[0].style.fill = "red";
-} */
+const AlimentosData= [
+{nombre:'usa', fill:hsl(128, 100%, 80%)},
+{nombre:'brasil', fill:hsl(128, 100%, 65%)},
+{nombre:'argentina', fill:hsl(128, 100%, 80%)},
+{nombre:'espana', fill:hsl(128, 100%, 65%)},
+{nombre:'irlanda', fill:hsl(128, 100%, 45%)},
+{nombre:'inglaterra', fill:hsl(128, 100%, 45%)},
+{nombre:'suiza', fill:hsl(128, 100%, 65%)},
+{nombre:'alemania', fill:hsl(128, 100%, 80%)},
+{nombre:'checa', fill:hsl(128, 100%, 45%)},
+{nombre:'japon', fill:hsl(128, 100%, 65%)},
+{nombre:'india', fill:hsl(128, 100%, 37%)},]
 
-/*
-var ar = document.getElementByClassName("Argentina");
-console.log(ar);
-ar.setAttribute("style","fill: red"); */
+
+function changeMap() {
+
+  const argentina = document.getElementsByClassName("Argentina");
+
+  for(let i = 0; i < ar.length;
+  i++){
+
+  argentina[i].setAttribute('style', 'fill: hsl(128, 100%, 65%)');
+
+  }
+
+}
