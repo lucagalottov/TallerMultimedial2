f485f26adc7fb7246f055bd0fe3fe212f4dd4724
lucagalottov <66344789+lucagalottov@users.noreply.github.com>
2021-12-05 18:14:10 -0300

    Add files via upload

--- Mapa Vegano/js/sliderMapa.js
+++ Mapa Vegano/js/sliderMapa.js
@@ -1,28 +1,89 @@
-var slider = document.getElementById("myRange");
+const paisesData = {
+2000: [
+{ nombre: "usa", style: "fill:hsl(128, 100%, 80%)"},
+{ nombre: "brasil", style: "fill:hsl(128, 100%, 65%)" },
+{ nombre: "argentina", style: "fill:hsl(128, 100%, 80%)" },
+{ nombre: "espana", style: "fill:hsl(128, 100%, 65%)" },
+{ nombre: "irlanda", style: "fill:hsl(128, 100%, 45%)" },
+{ nombre: "inglaterra", style: "fill:hsl(128, 100%, 45%)" },
+{ nombre: "suiza", style: "fill:hsl(128, 100%, 65%)"},
+{ nombre: "alemania", style: "fill:hsl(128, 100%, 80%)" },
+{ nombre: "checa", style: "fill:hsl(128, 100%, 45%)"},
+{ nombre: "japon", style: "fill:hsl(128, 100%, 65%)"},
+{ nombre: "india", style: "fill:hsl(128, 100%, 37%)"},
+],
+2005: [
+{ nombre: "usa", style: "fill:hsl(128, 100%, 65%)"},
+{ nombre: "brasil", style: "fill:hsl(128, 100%, 65%)" },
+{ nombre: "argentina", style: "fill:hsl(128, 100%, 65%)" },
+{ nombre: "espana", style: "fill:hsl(128, 100%, 65%)" },
+{ nombre: "irlanda", style: "fill:hsl(128, 100%, 45%)" },
+{ nombre: "inglaterra", style: "fill:hsl(128, 100%, 45%)" },
+{ nombre: "suiza", style: "fill:hsl(128, 100%, 65%)"},
+{ nombre: "alemania", style: "fill:hsl(128, 100%, 65%)" },
+{ nombre: "checa", style: "fill:hsl(128, 100%, 37%)"},
+{ nombre: "japon", style: "fill:hsl(128, 100%, 65%)"},
+{ nombre: "india", style: "fill:hsl(128, 100%, 23%)"},
+],
+2010: [
+{ nombre: "usa", style: "fill:hsl(128, 100%, 45%)"},
+{ nombre: "brasil", style: "fill:hsl(128, 100%, 45%)" },
+{ nombre: "argentina", style: "fill:hsl(128, 100%, 45%)" },
+{ nombre: "espana", style: "fill:hsl(128, 100%, 45%)" },
+{ nombre: "irlanda", style: "fill:hsl(128, 100%, 37%)" },
+{ nombre: "inglaterra", style: "fill:hsl(128, 100%, 37%)" },
+{ nombre: "suiza", style: "fill:hsl(128, 100%, 45%)"},
+{ nombre: "alemania", style: "fill:hsl(128, 100%, 45%)" },
+{ nombre: "checa", style: "fill:hsl(128, 100%, 37%)"},
+{ nombre: "japon", style: "fill:hsl(128, 100%, 65%)"},
+{ nombre: "india", style: "fill:hsl(128, 100%, 11%)"},
+],
+2021: [
+{ nombre: "usa", style: "fill:hsl(128, 100%, 23%)"},
+{ nombre: "brasil", style: "fill:hsl(128, 100%, 23%)" },
+{ nombre: "argentina", style: "fill:hsl(128, 100%, 11%)" },
+{ nombre: "espana", style: "fill:hsl(128, 100%, 37%)" },
+{ nombre: "irlanda", style: "fill:hsl(128, 100%, 11%)" },
+{ nombre: "inglaterra", style: "fill:hsl(128, 100%, 23%)" },
+{ nombre: "suiza", style: "fill:hsl(128, 100%, 37%)"},
+{ nombre: "alemania", style: "fill:hsl(128, 100%, 45%)" },
+{ nombre: "checa", style: "fill:hsl(128, 100%, 23%)"},
+{ nombre: "japon", style: "fill:hsl(128, 100%, 45%)"},
+{ nombre: "india", style: "fill:hsl(128, 100%, 11%)"},
+],
+};
 
-const AlimentosData= [
-{nombre:'usa', fill:hsl(128, 100%, 80%)},
-{nombre:'brasil', fill:hsl(128, 100%, 65%)},
-{nombre:'argentina', fill:hsl(128, 100%, 80%)},
-{nombre:'espana', fill:hsl(128, 100%, 65%)},
-{nombre:'irlanda', fill:hsl(128, 100%, 45%)},
-{nombre:'inglaterra', fill:hsl(128, 100%, 45%)},
-{nombre:'suiza', fill:hsl(128, 100%, 65%)},
-{nombre:'alemania', fill:hsl(128, 100%, 80%)},
-{nombre:'checa', fill:hsl(128, 100%, 45%)},
-{nombre:'japon', fill:hsl(128, 100%, 65%)},
-{nombre:'india', fill:hsl(128, 100%, 37%)},]
+function changeMap(fecha) {
+
+// En el parámetro fecha recibiremos el año que se haya seleccionado en el slider
+
+// Con esa fecha nos traemos los datos que necesitamos
+
+const dataFecha = paisesData[fecha];
 
 
-function changeMap() {
 
-  const argentina = document.getElementsByClassName("Argentina");
 
-  for(let i = 0; i < ar.length;
-  i++){
+// Ahora la variable dataFecha tendrá el listado de países con su color para el año que necesitamos
 
-  argentina[i].setAttribute('style', 'fill: hsl(128, 100%, 65%)');
+// Vamos loopeando por cada país y le cambiamos el color a los <path> que correspondan
 
-  }
+for (let j = 0; j < dataFecha.length; i++) {
+
+// seleccionamos todos los vectores (<path>) que correspondan al país
+
+// Para que esto funcione es necesario que la key "nombre" en el array de datos se corresponda con la clase que tienen los vectores en el mapa
+
+const vectoresPais = document.getElementsByClassName(dataFecha[j].usa);
+
+
+for (let i = 0; i < vectoresPais.length;
+i++) {
+
+vectoresPais[i].setAttribute("style", dataFecha[j].'fill:hsl(128, 100%, 65%)');
+
+}
+
+}
 
 }
